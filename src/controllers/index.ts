97d87408c03237dbc11d99773bb37a9b1bbfa5da
Bhaven Kakade <thisisbhaven@gmail.com>
2024-07-12 22:14:37 +0530

    create controller index for easy imports

---
+++ src/controllers/index.ts
@@ -0,0 +1,4 @@
+export { default as historyController } from './history.controller';
+export { default as metricsController } from './metrics.controller';
+export { default as savedContractController } from './savedContract.controller';
+export { default as userController } from './user.controller';
