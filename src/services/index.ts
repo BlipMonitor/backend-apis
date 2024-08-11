86c2754ebac30f7a4d2824bd6bb2296b5ab652ac
Bhaven Kakade <thisisbhaven@gmail.com>
2024-08-11 22:55:45 +0530

    create service indexer

---
+++ src/services/index.ts
@@ -0,0 +1,6 @@
+export { default as alertService } from './alert.service';
+export { default as emailService } from './email.service';
+export { default as historyService } from './history.service';
+export { default as metricsService } from './metrics.service';
+export { default as savedContractService } from './savedContract.service';
+export { default as userService } from './user.service';
