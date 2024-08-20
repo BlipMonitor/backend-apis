1a810d0a5c730b97c2e9e4e916613ef629e05607
Bhaven Kakade <thisisbhaven@gmail.com>
2024-08-20 22:59:22 +0530

    time range types

---
+++ src/types/timeRange.ts
@@ -0,0 +1,18 @@
+/**
+ * Time range for fetching metrics data
+ */
+export enum TimeRange {
+  HOUR_1 = 'HOUR_1',
+  HOUR_3 = 'HOUR_3',
+  HOUR_6 = 'HOUR_6',
+  HOUR_12 = 'HOUR_12',
+  DAY_1 = 'DAY_1',
+  DAY_3 = 'DAY_3',
+  WEEK_1 = 'WEEK_1',
+  WEEK_2 = 'WEEK_2',
+  MONTH_1 = 'MONTH_1',
+  MONTH_3 = 'MONTH_3',
+  MONTH_6 = 'MONTH_6',
+  YEAR_1 = 'YEAR_1',
+  ALL_TIME = 'ALL_TIME'
+}
