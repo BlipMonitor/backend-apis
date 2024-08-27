f568c29482632515f0c0799e15a803e7a44451ce
Bhaven Kakade <thisisbhaven@gmail.com>
2024-08-27 23:02:39 +0530

    make validation indexer

---
+++ src/validations/index.ts
@@ -0,0 +1,4 @@
+export { default as historyValidation } from './history.validation';
+export { default as metricsValidation } from './metrics.validation';
+export { default as savedContractValidation } from './savedContract.validation';
+export { default as userValidation } from './user.validation';
